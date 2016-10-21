4699a10c361952fd3fef361d6229fef2cf69c116
blankshsh <blankshsh@qq.com>
2016-10-21 22:57:46 +0800

    更新login—_JS说明文件

--- Login_js/README.md
+++ Login_js/README.md
@@ -1,5 +1,18 @@
 
 #调用LoginBox 
+###demo实现内容：
+###点击登录按钮：显示或隐藏登录框
+###关闭登录框：点击确定、取消或者页面
+###拖动效果：点击登录框的标题，便能拖动页面；
+###细节处理：  1阻止点击事件的冒泡传递
+###            2计算left top的最大最小值
+###             设置拖动登录框无法超出显示页面的边距
+###            3处理onmouseup后不解绑事件就会造成拖动bug
+###            4处理拖动时鼠标超出页面外时误触页面的click事件
+###             处理方法为mouseup后解绑click事件
+###             并设置setTimeout在短时间内在重新绑定click事件
+###            5在页面大小发生变化时 重写元素的位置
+
 ##调用LoginBox.init()就能使用登录弹窗组件。
 ###此弹窗由一个h1标题栏，两个span构建的按钮组成。
 ###各个接口对应的元素
@@ -10,32 +23,43 @@
 ###btnSure>>确认按钮
 ###btnCancel>>取消按钮
 ###弹窗组件已定义基础样式，也可复写样式。
-```ruby  
+// ```ruby  
 
 1)	直接使用LoginBox.init();创建弹框
 2)  使用接口自定义样式
     所有内容均有默认样式，可只选择需要的接口
     LoginBox.init({
-        name:"blank",
-        box:{
-            width:500,
-            height:600
-        },
-        span:{
-            width:30,
-            height:30
-        },
-        title: {
-            text:"登录框"
-        },
-        btnSure: {
-            text:"确定"
-            //"text-align": "left",
-            //text:"确定登录框登录框登录框登录框登录框"
-        },
-        btnCancel:{
-            text:"错误"
-        }
-    })
+    name: "blank",//name接口为元素组合创建不重复的id
+    box: {
+        //css 以""方式写入需要自写的样式
+        css:"width:500px;height: 600px"
+    },
+    span: {
+        css:"width:300px;height:600px"
+    },
+    title: {
+        //text接口 可以为能显示文本的内容创建自定义的文本
+        text: "登录框"
+    },
+    btnSure: {
+        text: "确定",
+        css:"text-align:left",
+        text:'确定登录框登录框登录框登录框登录框',
+    },
+    btnCancel: {
+        text: "错误"
+    }
+})
 
-``` 
+    JS说明
+    单例模式封装
+    /*
+        init() //创建获取传输的内容
+        base()//创建添加页面元素
+        bind() //绑定页面元素
+        assignment() //为页面元素添加样式
+        click()//绑定点击事件
+        move() //点击拖动事件
+    */
+   
+// ``` 
--- Login_js/build/js/function.js
+++ Login_js/build/js/function.js
@@ -2,24 +2,17 @@ function g_i(name) {
     return document.getElementById(name);
 }
 
-function g_c(name) {
-    return document.getElementsByClassName(name);
-}
-
-function g_t(name) {
-    return document.getElementsByTagName(name);
-}
-
 function create(name) {
     return document.createElement(name);
 }
 var LoginBox = {
     /*
-        init //组件（创建内容）
-        bind(); //绑定（绑定元素）
-        assignment(); //创建样式（为元素添加样式）
-        click(); //点击事件
-        move(); //移动事件
+        init() //创建获取传输的内容
+        base()//创建页面元素
+        bind() //绑定元素
+        assignment() //创建样式（为元素添加样式）
+        click()//点击事件
+        move() //移动事件
     */
     init: function(cfg) {
         cfg = cfg || {};
@@ -35,13 +28,13 @@ var LoginBox = {
         // console.log(cfg.btnSure);
         // console.log(cfg.btnCancel);
         var me = this;
-        me.base(cfg); //组件（创建内容）
+        //运行创建内容函数
+        me.base(cfg);
     },
     base: function(cfg) {
-        //console.log(name);
         var me = this;
         name_ = cfg.name;
-        //console.log(name_);
+        //创建元素
         var loginbox_ = create('div'),
             h1_ = create('h1'),
             btn_sure = create('span'),
@@ -50,14 +43,16 @@ var LoginBox = {
         h1_.id = name_ + "hold";
         btn_sure.id = name_ + "btnSure";
         btn_cancel.id = name_ + "btnCancel";
+        //添加元素
         loginbox_.appendChild(h1_);
         loginbox_.appendChild(btn_sure);
         loginbox_.appendChild(btn_cancel);
         document.body.appendChild(loginbox_);
-        //console.log(loginbox_);
+        //运行绑定函数（绑定元素）
         me.bind(name_, cfg);
     },
     bind: function(name_, cfg) {
+        //绑定元素
         var me = this;
         me.box = g_i(name_ + 'loginbox');
         me.title = g_i(name_ + "hold");
@@ -65,39 +60,41 @@ var LoginBox = {
         me.btnCancel = g_i(name_ + "btnCancel");
         me.span = me.box.getElementsByTagName('span');
         me.loginbtn = g_i("login");
-        //aba=me.box.childNodes[0].className;
-        // console.log(me.box);
-        // console.log(me.title);
-        // console.log(me.btnSure);
-        // console.log(me.btnCancel);
-        // console.log(me.span);
-        // console.log(me.loginbtn);
+        //运行添加样式函数
         me.assignment(name_, cfg);
     },
     assignment: function(name_, cfg) {
+        //添加样式
         var me = this;
-        me.box.style.cssText += "display:none;width: 500px;height: 200px;background-color: #f0f0f0;border-radius: 10px;position: fixed;left: 50%;top: 50%;transform: translate(-50%, -50%);min-width: 500px;min-height: 200px";
-        me.title.style.cssText += "display:block;text-align:center;padding:10px;line-height:40px;cursor:move;-webkit-user-select:none;font-weight:normal;font-size:20px";
+        me.box.style.cssText += "display:none;width: 500px;height: 200px;background-color: #f0f0f0;border-radius: 15px;position: fixed;left: 50%;top: 50%;transform: translate(-50%, -50%);min-width: 500px;min-height: 200px";
+        me.title.style.cssText += "margin:0px;display:block;text-align:center;padding:15px;line-height:40px;cursor:move;-webkit-user-select:none;font-weight:normal;font-size:20px";
         var span_ = me.span;
         for (var i = 0; i < span_.length; i++) {
-            span_[i].style.cssText += "white-space: nowrap;text-overflow:ellipsis;width: 100px;height: 40px;max-width:180px;max-height:60px;display: inline-block;border-radius: 10px;text-align: center;line-height: 40px;cursor: pointer;-webkit-user-select: none;min-width: 100px;min-height: 40px;overflow: hidden";
+            span_[i].style.cssText += "overflow:hidden;white-space: nowrap;text-overflow:ellipsis;width: 100px; height: 40px;max-width:180px;max-height:60px;display: inline-block; text-align: center;cursor: pointer;-webkit-user-select: none;min-width: 100px;min-height: 40px;overflow: hidden；font-size:14px;vertical-align: middle;border-radius:10px;";
+
         }
-        me.btnSure.style.cssText += "color: #fff;background-color: #227700;position: absolute;left: 50%;top: 60%;transform: translate(-100%);margin-left: -50px";
-        me.btnCancel.style.cssText += "color: #fff;background-color: #ff0000;position: absolute;left: 50%;top: 60%;margin-left: 50px";
+        me.btnSure.style.cssText += "color: #fff;background-color: #357ebd;border-color: #428bca;position: absolute;left: 50%;top: 60%;transform: translate(-100%);margin-left: -50px";
+        me.btnCancel.style.cssText += "color: #fff; background-color: #d9534f; border-color: #d43f3a;position: absolute;left: 50%;top: 60%;margin-left: 50px";
         me.box.style.cssText += cfg.box.css;
         for (var i = 0; i < me.span.length; i++) {
-            //console.log(me.span[i]);
-            //console.log(cfg.span.css);
             me.span[i].style.cssText += cfg.span.css;
+            var lineheight_ = Number(me.span[i].style.height.replace(/px$/, ''));
+            var maxheight_ = Number(me.span[i].style.maxHeight.replace(/px$/, ''));
+            // console.log(maxheight_);
+            lineheight_ < maxheight_ ? span_[i].style.lineHeight = lineheight_ + "px" : span_[i].style.lineHeight = maxheight_ + "px";
         }
+        //获取用户新建的样式 并覆盖样式表
         me.btnSure.style.cssText += cfg.btnSure.css;
         me.btnCancel.style.cssText += cfg.btnCancel.css;
+        //判断用户是否自定义文本内容
         !cfg.title.text ? me.title.textContent = "欢迎登录" : me.title.textContent = cfg.title.text;
         !cfg.btnSure.text ? me.btnSure.textContent = "登录" : me.btnSure.textContent = cfg.btnSure.text;
         !cfg.btnCancel.text ? me.btnCancel.textContent = "取消" : me.btnCancel.textContent = cfg.btnCancel.text;
+        //运行绑定点击事件函数
         me.click(name_, cfg);
     },
     click: function(name_, cfg) {
+        //点击事件
         var me = this;
         me.loginbtn.addEventListener('click', function() {
             //处理冒泡
@@ -118,76 +115,81 @@ var LoginBox = {
         })
         document.onclick = function() {
             me.box.style.display = "none";
-        }
+        };
+        //运行点击拖动函数
         me.move(name_, cfg);
     },
     move: function(name_, cfg) {
         //点击拖动事件
         var me = this;
+        //创建储存位移值元素
         var clientX, clientY, moving;
         var mouseDown = function(event) {
             event = event || window.event;
             clientX = event.clientX;
             clientY = event.clientY;
             moving = !0;
+            //鼠标move 运行mouseMove()函数
             document.onmousemove = function(e) {
-                mouseMove(e);
-            }
+                    mouseMove(e);
+                }
+                //鼠标up  解除绑定跟onmousemove onmouseup
+                //解除document.onclick事件 处理移动范围过大造成的误触隐藏事件
+                //使用延时器 重新添加document.onclick隐藏box的事件
             document.onmouseup = function(e) {
                 event = event || window.event;
                 document.onmouseup = null;
                 document.onmousemove = null;
                 document.onclick = null;
                 setTimeout(function() {
-                    document.onclick = function() {
-                        me.box.style.display = "none";
-                    }
-                }, 50)
-                // me.title.removeEventListener('mousedown', mouseDownHandler);
-                // me.title.removeEventListener('mousemove', mouseMoveHandler);
-                // me.title.removeEventListener('mouseup', mouseUpHandler);
+                        document.onclick = function() {
+                            me.box.style.display = "none";
+                        }
+                    }, 50)
+                    // me.title.removeEventListener('mousedown', mouseDownHandler);
+                    // me.title.removeEventListener('mousemove', mouseMoveHandler);
+                    // me.title.removeEventListener('mouseup', mouseUpHandler);
             }
-        }
+        };
         var mouseMove = function(event) {
+            //拖动事件的主函数
             if (!moving) return;
             event = event || window.event;
+            //新建拖动后的元素位置
             var newClientX = event.clientX;
             var newClientY = event.clientY;
+            //获取元素离top left的距离
             var top = me.box.offsetTop;
             var left = me.box.offsetLeft;
+            //计算元素应该距离left和top的最小值
             var min_t = (Number(me.box.style.height.replace(/px$/, '')) / 2 || 0);
             var min_l = (Number(me.box.style.width.replace(/px$/, '')) / 2 || 0);
+            //计算元素应该距离left和top的最大值
             var max_t = window.innerHeight - (Number(me.box.style.height.replace(/px$/, '')) / 2 || 0);
             var max_l = window.innerWidth - (Number(me.box.style.width.replace(/px$/, '')) / 2 || 0);
+            //赋值最终的拖动距离
             newleft = left + (newClientX - clientX);
             newtop = top + (newClientY - clientY);
-            console.log(newleft);
-            if (newleft < min_l) {
-                me.box.style.left = min_l + "px";
-                console.log(me.box.style.left);
-            } else if (newleft > max_l) {
-                me.box.style.left = max_l + "px";
-
-            } else {
-                me.box.style.left = newleft + "px"
-            }
-            if (newtop < min_t) {
-                me.box.style.top = min_t + "px"
-            } else if (newtop > max_t) {
-                me.box.style.top = max_t + "px";
-            } else {
-                me.box.style.top = newtop + "px"
-            }
+            //console.log(newleft);
+            //判读输出  box.left box.top必须在最小值和最大值之间
+            var endleft = Math.max(newleft, min_l);
+            var endleft = Math.min(endleft, max_l);
+            var endtop = Math.max(newtop, min_t);
+            var endtop = Math.min(endtop, max_t);
+            //赋值最终计算结果给被拖动的元素
+            me.box.style.left = endleft + "px";
+            me.box.style.top = endtop + "px";
+            //新位置记录赋值
             clientX = newClientX;
             clientY = newClientY;
-        }
+        };
+        //绑定鼠标拖动的监听事件
         me.title.addEventListener('mousedown', mouseDown);
-        //me.documentclick();
+        //当窗口改变时 运行重排列
         window.onresize = function(e) {
-                console.log(me.box);
-                me.box.style.left = "50%";
-                me.box.style.top = "50%";
+            //console.log(me.box);
+            me.box.style.left = "50%";
+            me.box.style.top = "50%";
         }
-    },
-
+    }
 }
